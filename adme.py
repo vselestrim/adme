80cbba7a9d32e4b3bf0bbc938de8c0ed2e44b5a8
vselestrim <108416333+vselestrim@users.noreply.github.com>
2022-07-30 13:37:11 -0300

    Create adme.py

---
+++ adme.py
@@ -0,0 +1,13 @@
+# install python3 https://www.python.org/downloads/ or 
+#  $ brew install python3
+# check if pip3 is installed:
+#  $ pip3 --version
+# install opencv:
+#  $ pip3 install opencv-python
+# download haarcascade face detect model:
+#  $ wget https://raw.githubusercontent.com/opencv/opencv/3.4/data/haarcascades/haarcascade_frontalface_default.xml
+# download the logo you want:
+#  $ wget https://logodownload.org/wp-content/uploads/2013/12/apple-logo-11.png
+# run following code:
+#  $ python3 adme.py
+
